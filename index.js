719b8d6be886487658661a73e641844f5dfd7be8
Sameer Mirza <israfil334@gmail.com>
2018-06-07 08:45:51 -0700

    asd

--- index.js
+++ index.js
@@ -4,31 +4,88 @@ import { Controller, Application } from './lib';
 import view, { render } from './lib/view'; // eslint-disable-line
 
 class TodoController extends Controller {
+  todos = [];
+  newTodo = '';
+
+  constructor() {
+    super();
+
+    this.onKeyDown = this.onKeyDown.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
 
   onKeyDown(e) {
     if (e.keyCode !== 13) {
       return;
     }
 
-    event.preventDefault();
+    e.preventDefault();
 
+    this.todos.push(this.newTodo);
+    this.newTodo = '';
+
+    this.index();
+  }
+
+  onChange(e) {
+    this.newTodo = e.target.value;
+  }
+
+  todoItems() {
+    const items = [];
+    for (let i = 0; i < this.todos.length; i++) {
+      items.push(
+        <li class="">
+          <div class="view">
+            <input class="toggle" type="checkbox" />
+            <label>{this.todos[i]}</label>
+            <button class="destroy"></button>
+          </div>
+        </li>
+      )
+    }
+
+    return (
+      <ul class="todo-list">
+        {items}
+      </ul>
+    );
+  }
+
+  main() {
+    if (!this.todos.length) {
+      return null;
+    } else {
+      return <section class="main">
+        <input
+          id="toggle-all"
+          class="toggle-all"
+          type="checkbox"
+          onchange={this.toggleAll}
+          checked={this.todos.length === 0}
+        />
+        <label htmlfor="toggle-all" />
+        {this.todoItems()}
+      </section>;
+    }
   }
 
   @render
   index() {
     return (
-      <div>
-        <header className="header">
+      <view>
+        <header class="header">
           <h1>todos</h1>
           <input
             class="new-todo"
             placeholder="What needs to be done?"
             value={this.newTodo}
-            onkeydown={this.handleNewTodoKeyDown}
-            onchange={this.handleChange}
+            onkeydown={this.onKeyDown}
+            onchange={this.onChange}
           />
         </header>
-      </div>
+        {this.main()}
+      </view>
     );
   }
 }
